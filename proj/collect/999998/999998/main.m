701720e994c8d17d51060195e6f54516a65f055a
邓江洲 <dengjiangzhouReal@gmail.com>
2021-08-29 14:37:21 +0800

    预处理指令

---
+++ proj/collect/999998/999998/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  999998
+//
+//  Created by Jz D on 2021/8/27.
+//
+
+#import <stdio.h>
+
+#define C 30
+
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        int a = 10;
+        int b = 20;
+        
+        printf("%d", a + b + C);
+    }
+    return 0;
+}
---
+++ proj/collect/README.md
@@ -0,0 +1,3 @@
+预处理指令
+
+#  clang -E main.m >> main_two.m
